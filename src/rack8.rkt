f3e61f943ec55c01dcb910bd649123f68bf016b2
Kewbish <kewbish@gmail.com>
2020-11-05 16:56:57 -0800

    Read and print as hex

--- src/rack8.rkt
+++ src/rack8.rkt
@@ -1,7 +1,11 @@
 #lang racket
 
 (define (readRom path)
+  (define loaded '())
   (for ([line (file->bytes path)])
-    (displayln line)))
+    (set! loaded (append loaded (list line))))
+  (for ([line loaded])
+    (printf "~X" line))  
+)
 
 (readRom "CONNECT4.ch8")
