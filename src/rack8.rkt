d48ceb80dcb015bb4c5a781ea8ab31c561d4c8af
Kewbish <kewbish@gmail.com>
2020-11-05 17:19:36 -0800

    Add state

--- src/rack8.rkt
+++ src/rack8.rkt
@@ -1,11 +1,19 @@
 #lang racket
 
-(define (readRom path)
+(struct chip8-state (memory [pc #:mutable] [stack #:mutable] regs [reg-i #:mutable]))
+
+(define (read-rom path)
   (define loaded '())
   (for ([line (file->bytes path)])
     (set! loaded (append loaded (list line))))
   (for ([line loaded])
     (printf "~X" line))  
+  loaded
 )
 
-(readRom "CONNECT4.ch8")
+(define (set-init path)
+  (define rom-bytes (read-rom path))
+  (define cur-state (chip8-state (make-bytes #x1000) #x200 '() (make-bytes 16) #x0))
+cur-state)
+
+(set-init "CONNECT4.ch8")
