711741018b6d432427f8856b1fb84cccecfebca4
Kewbish <kewbish@gmail.com>
2020-11-06 19:16:03 -0800

    Add cells printing

--- src/rack8.rkt
+++ src/rack8.rkt
@@ -1,5 +1,4 @@
 #lang racket
-(require srfi/25)
 
 (struct chip8-state (memory [pc #:mutable] [stack #:mutable] registers [reg-i #:mutable]))
 
@@ -10,29 +9,40 @@
 
 (define (set-init path)
   (define rom-bytes (read-rom path))
+  ; create a chip8 state, set most to empty vals
   (define state (chip8-state (make-bytes #x1000) #x200 '() (make-bytes 16) #x0))
+  ; load the program and fontset into the memory
   (bytes-copy! (chip8-state-memory state) #x200 rom-bytes)
-  (bytes-copy! (chip8-state-memory state) 0 #"\360\220\220\220\360")
-  (bytes-copy! (chip8-state-memory state) 5 #"\040\140\040\040\160")
-  (bytes-copy! (chip8-state-memory state) 10 #"\360\020\360\200\360")
-  (bytes-copy! (chip8-state-memory state) 15 #"\360\020\360\020\360")
-  (bytes-copy! (chip8-state-memory state) 20 #"\220\220\360\020\020")
-  (bytes-copy! (chip8-state-memory state) 25 #"\360\200\360\020\360")
-  (bytes-copy! (chip8-state-memory state) 30 #"\360\200\360\220\360")
-  (bytes-copy! (chip8-state-memory state) 35 #"\360\020\040\200\200")
-  (bytes-copy! (chip8-state-memory state) 40 #"\360\220\360\220\360")
-  (bytes-copy! (chip8-state-memory state) 45 #"\360\220\360\020\360")
-  (bytes-copy! (chip8-state-memory state) 50 #"\360\220\360\220\220")
-  (bytes-copy! (chip8-state-memory state) 55 #"\340\220\340\220\340")
-  (bytes-copy! (chip8-state-memory state) 60 #"\360\200\200\200\360")
-  (bytes-copy! (chip8-state-memory state) 65 #"\340\220\220\220\340")
-  (bytes-copy! (chip8-state-memory state) 70 #"\360\200\360\200\360")
-  (bytes-copy! (chip8-state-memory state) 75 #"\360\200\360\200\200")
+  (bytes-copy! (chip8-state-memory state) 50 #"\360\220\220\220\360")
+  (bytes-copy! (chip8-state-memory state) 55 #"\040\140\040\040\160")
+  (bytes-copy! (chip8-state-memory state) 60 #"\360\020\360\200\360")
+  (bytes-copy! (chip8-state-memory state) 65 #"\360\020\360\020\360")
+  (bytes-copy! (chip8-state-memory state) 70 #"\220\220\360\020\020")
+  (bytes-copy! (chip8-state-memory state) 75 #"\360\200\360\020\360")
+  (bytes-copy! (chip8-state-memory state) 80 #"\360\200\360\220\360")
+  (bytes-copy! (chip8-state-memory state) 85 #"\360\020\040\200\200")
+  (bytes-copy! (chip8-state-memory state) 90 #"\360\220\360\220\360")
+  (bytes-copy! (chip8-state-memory state) 95 #"\360\220\360\020\360")
+  (bytes-copy! (chip8-state-memory state) 100 #"\360\220\360\220\220")
+  (bytes-copy! (chip8-state-memory state) 105 #"\340\220\340\220\340")
+  (bytes-copy! (chip8-state-memory state) 110 #"\360\200\200\200\360")
+  (bytes-copy! (chip8-state-memory state) 115 #"\340\220\220\220\340")
+  (bytes-copy! (chip8-state-memory state) 120 #"\360\200\360\200\360")
+  (bytes-copy! (chip8-state-memory state) 125 #"\360\200\360\200\200")
 state)
 
+; create state object, will be used thruo interpreting
 (define state (set-init "CONNECT4.ch8"))
 (define memory (chip8-state-memory state))
-(define graphics (make-vector 2048))
 (define keys (make-bytes 16))
+; graphics -> 2d vector (have to do some interesting vector layering)
+(define graphics (make-vector 64 (make-vector 32 0)))
+; helper refs and sets, will get values
+(define (2d-ref 2d-vec x y)
+  (vector-ref (vector-ref 2d-vec x) y))
+(define (2d-set! 2d-vec x y)
+  (vector-set! (vector-ref 2d-vec x) y))
 
-(set-init "CONNECT4.ch8")
+(define cells (bytes->list memory))
+(for [cell (cells)]
+  printf "~X" cell)
