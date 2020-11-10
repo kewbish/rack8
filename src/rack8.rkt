59b9c0e443b0d8ba37717e8fa7b35883ae69a11c
Kewbish <kewbish@gmail.com>
2020-11-10 08:12:25 -0800

    Fix loop - move to cond

--- src/rack8.rkt
+++ src/rack8.rkt
@@ -1,8 +1,7 @@
 #lang racket
 
 (struct chip8-state (memory [pc #:mutable] [stack #:mutable] registers [reg-i #:mutable]))
-(define (read-rom path) (define loaded (file->bytes path))
-  loaded
+(define (read-rom path) (define loaded (file->bytes path)) loaded
 )
 
 (define (set-init path)
@@ -11,22 +10,22 @@
   (define state (chip8-state (make-bytes #x1000) #x200 '() (make-bytes 16) #x0))
   ; load the program and fontset into the memory
   (bytes-copy! (chip8-state-memory state) #x200 rom-bytes)
-  (bytes-copy! (chip8-state-memory state) 50 #"\360\220\220\220\360")
-  (bytes-copy! (chip8-state-memory state) 55 #"\040\140\040\040\160")
-  (bytes-copy! (chip8-state-memory state) 60 #"\360\020\360\200\360")
-  (bytes-copy! (chip8-state-memory state) 65 #"\360\020\360\020\360")
-  (bytes-copy! (chip8-state-memory state) 70 #"\220\220\360\020\020")
-  (bytes-copy! (chip8-state-memory state) 75 #"\360\200\360\020\360")
-  (bytes-copy! (chip8-state-memory state) 80 #"\360\200\360\220\360")
-  (bytes-copy! (chip8-state-memory state) 85 #"\360\020\040\200\200")
-  (bytes-copy! (chip8-state-memory state) 90 #"\360\220\360\220\360")
-  (bytes-copy! (chip8-state-memory state) 95 #"\360\220\360\020\360")
-  (bytes-copy! (chip8-state-memory state) 100 #"\360\220\360\220\220")
-  (bytes-copy! (chip8-state-memory state) 105 #"\340\220\340\220\340")
-  (bytes-copy! (chip8-state-memory state) 110 #"\360\200\200\200\360")
-  (bytes-copy! (chip8-state-memory state) 115 #"\340\220\220\220\340")
-  (bytes-copy! (chip8-state-memory state) 120 #"\360\200\360\200\360")
-  (bytes-copy! (chip8-state-memory state) 125 #"\360\200\360\200\200")
+  (bytes-copy! (chip8-state-memory state) 0 #"\360\220\220\220\360")
+  (bytes-copy! (chip8-state-memory state) 5 #"\040\140\040\040\160")
+  (bytes-copy! (chip8-state-memory state) 10 #"\360\020\360\200\360")
+  (bytes-copy! (chip8-state-memory state) 15 #"\360\020\360\020\360")
+  (bytes-copy! (chip8-state-memory state) 20 #"\220\220\360\020\020")
+  (bytes-copy! (chip8-state-memory state) 25 #"\360\200\360\020\360")
+  (bytes-copy! (chip8-state-memory state) 30 #"\360\200\360\220\360")
+  (bytes-copy! (chip8-state-memory state) 35 #"\360\020\040\200\200")
+  (bytes-copy! (chip8-state-memory state) 40 #"\360\220\360\220\360")
+  (bytes-copy! (chip8-state-memory state) 45 #"\360\220\360\020\360")
+  (bytes-copy! (chip8-state-memory state) 50 #"\360\220\360\220\220")
+  (bytes-copy! (chip8-state-memory state) 55 #"\340\220\340\220\340")
+  (bytes-copy! (chip8-state-memory state) 60 #"\360\200\200\200\360")
+  (bytes-copy! (chip8-state-memory state) 65 #"\340\220\220\220\340")
+  (bytes-copy! (chip8-state-memory state) 70 #"\360\200\360\200\360")
+  (bytes-copy! (chip8-state-memory state) 75 #"\360\200\360\200\200")
 state)
 
 ; create state object, will be used thruo interpreting
@@ -64,38 +63,38 @@ state)
 ; registers and counters
 (define (get-pc) (chip8-state-pc state))
 (define (set-pc v) (set-chip8-state-pc! state v))
-(define (incre-pc) (set-chip8-state-pc! state (+ (get-pc) 2)))
+(define (incre-pc) (set-pc (+ (get-pc) 2)))
 (define (get-reg n) (bytes-ref (chip8-state-registers state) n))
 (define (set-reg n v) (bytes-set! (chip8-state-registers state) n v))
-(define (get-regi) (bytes-ref (chip8-state-reg-i state)))
-(define (set-regi v) (bytes-set! (set-chip8-state-reg-i! state v)))
+(define (get-regi) (chip8-state-reg-i state))
+(define (set-regi v) (set-chip8-state-reg-i! state v))
 
 ; emulate one opcode
 (define (cycle)
-  (define inst (+ (* (bytes-ref memory (get-pc)) #x100) (bytes-ref memory (+ (get-pc) 1))))
+  (printf "~x" (bytes-ref memory (get-pc)))
+  (define inst (+ (* (bytes-ref memory (get-pc)) #x100) (bytes-ref memory (+ 1 (get-pc)))))
   (define (masked mask) (bitwise-and inst mask))
   ; ? denotes a boolean returner
-  (define (hex-form? mask value) (= (masked mask) value))
+  (define (hex-op? mask value) (= (masked mask) value))
   ; variables
   (define nnn (masked #x0fff))
   (define kk (masked #x00ff))
   (define n (masked #xf000))
-  (define x (masked #x0f00))
-  (define y (masked #x00f0))
-  (match n 
-         [#x0000
-          (cond
-            [(= inst 224) (printf "CLEAR SCREEN")]
-            [(= inst 238) (printf "RETURN")]
-            [else (printf "CALL AT ~a" nnn)])]
-         [#x1000 (printf "JUMP TO ADDRESS ~a" nnn)]
-         [#x2000 (printf "SUBROUTINE AT ~a" nnn)]
-         [#x3000 (printf "SKIP IF EQUAL ~a" (= (get-reg x) kk))]
-         [#x4000 (printf "SKIP IF INEQUAL ~a" (not (= (get-reg x) kk)))]
-         [#x5000 (printf "SKIP IF 2EQUAL ~a" (= (get-reg x) (get-reg y)))]
-         [#x6000 (printf "SET REG ~a" x)]
-         [#x7000 (printf "ADD ~a TO REG ~a" kk x)]
-         [n (printf "[~a: ~x|~a] " (get-pc) inst inst)])
+  (define x (get-reg (/ (masked #x0f00) #x100)))
+  (define y (get-reg (/ (masked #x00f0) #x10)))
+  ; opcode cycle loop
+  (cond
+    [(= inst #x00e0) (printf "CLEAR SCREEN")]
+    [(= inst #x00ee) (printf "RETURN")]
+    [(and (= n #x0000) (not (= inst #x0000))) (printf "CALL AT ~a" nnn)]
+    [(= n #x1000) (printf "JUMP TO ADDRESS ~a" nnn)]
+    [(= n #x2000) (printf "SUBROUTINE AT ~a" nnn)]
+    [(= n #x3000) (printf "SKIP IF EQUAL ~a" (= (get-reg x) kk))]
+    [(= n #x4000) (printf "SKIP IF INEQUAL ~a" (not (= (get-reg x) kk)))]
+    [(= n #x5000) (printf "SKIP IF 2EQUAL ~a" (= (get-reg x) (get-reg y)))]
+    [(= n #x6000) (printf "SET REG ~a" x)]
+    [(= n #x7000) (printf "ADD ~a TO REG ~a" kk x)]
+    [else (printf "[~a: ~x|~a] " (get-pc) inst inst)])
   (incre-pc)
 )
 
